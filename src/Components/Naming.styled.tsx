adf2dd5aa73fbd2d6d4b4a310386db76f49f0b89
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-16 12:27:47 +0300

    add Naming.tsx

---
+++ src/Components/Naming.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Naming = styled.div`
+    color: #ABB3BA;
+    font-family: Inter, sans-serif;
+    font-size: 11px;
+    font-weight: 500;
+    line-height: 20px;
+    order: 0;
+    align-self: flex-start;
+    margin-left: 200px
+`
