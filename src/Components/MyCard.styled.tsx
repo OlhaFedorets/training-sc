0d5c42230aef0e1945651516d06198e1f7774fb0
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-16 12:27:37 +0300

    add MyCard.tsx

---
+++ src/Components/MyCard.styled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const MyCardStyled = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 350px;
+    top: -317px;
+    left: -409px;
+    border-radius: 15px;
+    background-color: white;
+    box-shadow: 0px 4px 20px 5px #0000001A;
+`
