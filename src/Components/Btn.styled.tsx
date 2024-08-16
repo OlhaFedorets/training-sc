36c83d48d69c133eaf44db47250fde275a335ccd
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-16 12:26:52 +0300

    add Btn.tsx

---
+++ src/Components/Btn.styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+type BtnPropsType = {
+    color?: string
+    fontColor?: string
+}
+
+export const Btn = styled.button<BtnPropsType>`
+    background-color: ${props => props.color};
+    width: 86px;
+    height: 30px;
+    top: 298px;
+    margin-left: 20px;
+    border-radius: 5px;
+    border: 2px solid #4E71FE;
+    font-family: Inter, sans-serif;
+    font-size: 11px;
+    font-weight: 500;
+    color: ${props => props.fontColor};
+`
