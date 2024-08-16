cdc96d2f8810a165f12058abeb1507d2dddd0cdd
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-16 12:27:13 +0300

    add Headline.tsx

---
+++ src/Components/Headline.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Headline = styled.h1`
+    width: 70px;
+    height: 19px;
+    top: 200px;
+    margin-left: 20px;
+    font-family: Inter, sans-serif;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 20px;
+    text-align: left;
+
+`
