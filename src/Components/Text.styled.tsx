02255e4259e67ecac75bf1b1277f564dc4602621
OlhaFedorets <fedoretsolha@gmail.com>
2024-08-16 12:28:03 +0300

    add Text.tsx

---
+++ src/Components/Text.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+    width: 260px;
+    height: 40px;
+    top: 239px;
+    margin-left: 20px;
+    font-family: Inter, sans-serif;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 20px;
+    text-align: left;
+    color: #ABB3BA;
+`
